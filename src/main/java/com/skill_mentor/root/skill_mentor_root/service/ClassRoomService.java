69b4c5dd2d9f136ea96f341e1e72e20c5b29d9e6
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-05-11 00:16:07 +0530

    CRUD operations for ClassRoom

---
+++ src/main/java/com/skill_mentor/root/skill_mentor_root/service/ClassRoomService.java
@@ -0,0 +1,52 @@
+package com.skill_mentor.root.skill_mentor_root.service;
+
+import com.skill_mentor.root.skill_mentor_root.dto.ClassRoomDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public interface ClassRoomService {
+    /**
+     * Creates a new classroom with the provided details.
+     *
+     * @param classRoomDTO the new classroom data
+     * @return the created {@link ClassRoomDTO} with generated ID
+     */
+    ClassRoomDTO createClassRoom(ClassRoomDTO classRoomDTO);
+
+    /**
+     * Retrieves all classrooms filtered by optional parameters.
+     * @return a list of {@link ClassRoomDTO} that match the filters
+     */
+    List<ClassRoomDTO> getAllClassRooms();
+
+    /**
+     * Finds a classroom by its ID.
+     *
+     * @param id the unique ID of the classroom
+     * @return the matching {@link ClassRoomDTO}, or {@code null} if not found
+     */
+    ClassRoomDTO findClassRoomById(Integer id);
+
+    /**
+     * Updates an existing classroom.
+     *
+     * @param classRoomDTO the updated classroom details
+     * @return the updated {@link ClassRoomDTO}, or {@code null} if the classroom does not exist
+     */
+    ClassRoomDTO updateClassRoom(ClassRoomDTO classRoomDTO);
+
+    /**
+     * Deletes a classroom by its ID.
+     *
+     * @param id the ID of the classroom to delete
+     * @return the deleted {@link ClassRoomDTO}, or {@code null} if not found
+     */
+    ClassRoomDTO deleteClassRoomById(Integer id);
+
+
+
+
+
+}
