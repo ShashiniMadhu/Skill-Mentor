fc7047f0ab661d4fef7ce24bec5b0da652ce6206
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-07-08 19:25:47 +0530

    Add exception

---
+++ src/main/java/com/skill_mentor/root/skill_mentor_root/exception/AuditException.java
@@ -0,0 +1,7 @@
+package com.skill_mentor.root.skill_mentor_root.exception;
+
+public class AuditException extends Exception{
+    public AuditException(String message, Throwable throwable){
+        super(message, throwable);
+    }
+}
